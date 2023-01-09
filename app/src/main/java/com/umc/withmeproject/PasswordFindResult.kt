e34082c068cdbf2c5c7a714dc98cbddcfc7dc4c6
MinWoo Lee <rkdxhd97@naver.com>
2023-01-09 17:49:51 +0900

    first commit

---
+++ app/src/main/java/com/umc/withmeproject/PasswordFindResult.kt
@@ -0,0 +1,11 @@
+package com.umc.withmeproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PasswordFindResult : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_find_result)
+    }
+}
